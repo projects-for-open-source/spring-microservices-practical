17d0847c067b2e9e9f79cb35a821a2ed0ad4dcdf
Richard Vu <richardvu.work@gmail.com>
2021-08-04 21:08:54 +0700

    Initial spring-microservices-practical-config-server project

---
+++ spring-microservices-practical-config-server/src/main/java/com/richard/application/ConfigServerApplication.java
@@ -0,0 +1,28 @@
+/**
+ * @Nickname: Richard
+ * @Email: richardvu.work@gmail.com
+ * @Skype: richardvu.work@gmail.com
+ * @Phone: (+84) 0935710974 - (+84) 0935810974
+ * @Country: Viet Nam
+ * @Year: 2021
+ * @LICENSE: DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ */
+package com.richard.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+/**
+ * @author richard
+ *
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
